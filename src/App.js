426d2c099b7a67022b5370213be35373edce2944
Rafael Silva <rafaeloliveirasilva05@gmail.com>
2018-01-24 23:41:05 -0200

    foi acrescentado estilo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import {
+  ScrollView,
+  StyleSheet,
   Platform,
   Text,
   View
+
 } from 'react-native';
 
 import {
@@ -33,10 +36,16 @@ export default class App extends Component<{}> {
           if (error) {
             return <Text>{error.message}</Text>;
           } else if (props) {
-
             return (
-              <View>
-                {props.players.map(item => <Post item={item} />)}
+              <View style={styles.container}>
+                <View style={styles.header}>
+                  <Text style={styles.headerText}>Jogadores do Chicago Bulls</Text>
+                </View>
+
+                <ScrollView contentContainerStyle={styles.repoList}>
+                  {props.players.map(item => <Post item={item} />)}
+                </ScrollView>
+
               </View>
             )
           }
@@ -47,4 +56,27 @@ export default class App extends Component<{}> {
   }
 }
 
+const styles = StyleSheet.create({
 
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+
+  header: {
+    height: (Platform.OS === 'ios') ? 70 : 50,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  headerText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  repoList: {
+    padding: 10,
+  },
+})
